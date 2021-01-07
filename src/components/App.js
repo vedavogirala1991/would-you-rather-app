c778763861699e8a1e3ae5cd1c42e7a7016a1b2f
Veda Vogirala <vedavogirala1991@users.noreply.github.com>
2021-01-07 07:45:35 +0530

    Would You Rather App - Beatifications need to remove few

--- src/components/App.js
+++ src/components/App.js
@@ -14,6 +14,7 @@ import QuestionPage from './QuestionPage'
 import NewQuestion from './NewQuestion'
 import LeaderBoard from './LeaderBoard'
 import Nav from './Nav'
+import bgimage from '../images/bgimage.jpg'
 
 class App extends Component {
   componentDidMount () {
