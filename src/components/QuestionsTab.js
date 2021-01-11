de031c5ff7d3d368b89b195a97a3c06adbcb6918
veda vogirala <veda.vogirala@gmail.com>
2021-01-12 01:16:21 +0530

    Would You Rather App - Making Questions Tab Stateless

--- src/components/QuestionsTab.js
+++ src/components/QuestionsTab.js
@@ -1,46 +1,34 @@
-import React,{Component} from 'react'
-import {connect} from 'react-redux'
 import Question from './Question'
 
-class QuestionsTab extends Component {
-  render () {
-    const {questionIds,className} = this.props
-    if(questionIds.length===0) {
-      let emptyText = 'You have answered all questions, add new question to answer.'
-      if(this.props.id==='Answered Questions') {
-        emptyText = 'You have not answered any question, start answering.'
-      }
+const QuestionsTab = (props) => {
 
-      return (
-        <div className='empty-question'>
-          <span>{emptyText}</span>
-        </div>
-      )
+  const {questionIds,className} = props
+  
+  if(questionIds.length===0) {
+    let emptyText = 'You have answered all questions, add new question to answer.'
+    if(this.props.id==='Answered Questions') {
+      emptyText = 'You have not answered any question, start answering.'
     }
 
     return (
-      <div className={className}>
-        <ul className='question'>
-            {questionIds &&
-              questionIds.map((id) =>(
-              <li key={id}>
-                <Question id={id}/>
-              </li>
-            ))}
-        </ul>
+      <div className='empty-question'>
+        <span>{emptyText}</span>
       </div>
     )
   }
+
+  return (
+    <div className={className}>
+      <ul className='question'>
+          {questionIds &&
+            questionIds.map((id) =>(
+            <li key={id}>
+              <Question id={id}/>
+            </li>
+          ))}
+      </ul>
+    </div>
+  )
 }
 
-const mapStateToProps = ({questions}, {questionIds,id,className}) => {
-  return {
-    questionIds : questionIds ? Object.keys(questions)
-      .sort((a,b) => questions[b].timestamp - questions[a].timestamp).filter((id) => questionIds.includes(id))
-      : null,
-    className,
-    id,
-  }
-}
-
-export default connect(mapStateToProps)(QuestionsTab)
+export default QuestionsTab
